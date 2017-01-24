d96d55de28340d61001ce5350cd1c62a7a324432
abdcfaferadfa <zxcvbn5412@gmail.com>
2017-01-24 09:59:39 +0800

    text change

--- app/SlideNav/SliderNav.ts
+++ app/SlideNav/SliderNav.ts
@@ -183,7 +183,7 @@ angular.module("Constants", []).constant("config", new Configuration())
                 canChangePolynomial : true,
             },
             {
-                additionalTitle : "S-Box",
+                additionalTitle : "S-Box computation",
                 showGFString : true,
                 canComputeInGFTwo : true,
                 messageForGFTwo : "Fixed for S-Box",
