dde08279d3977fd36d77228ab1b096faf755d96a
Zhou <zhou0235@e.ntu.edu.sg>
2017-02-08 19:29:29 +0800

    feedback

--- app/components/version/version.js
+++ app/components/version/version.js
@@ -3,5 +3,5 @@ angular.module('myApp.version', [
     'myApp.version.interpolate-filter',
     'myApp.version.version-directive'
 ])
-    .value('version', '0.9');
+    .value('version', '1.5');
 //# sourceMappingURL=version.js.map
